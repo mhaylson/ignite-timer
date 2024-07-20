a3bc75c6e1cb2c01da763d83cad927629516975d
Mhaylson Whandson <mhaylson@m2smart.com.br>
2024-07-19 20:19:59 -0400

    Added Styled Components
    Added.
    
    Signed-off-by: Mhaylson Whandson <mhaylson@m2smart.com.br>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Button />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
     </>
   )
 }
