a28e59b8f1714a7293849d6f1389c5295da702d3
Mhaylson Whandson <mhaylson@m2smart.com.br>
2024-07-20 10:05:44 -0400

    Colors & Fonts
    Added the colors and Roboto Fonts
    
    Signed-off-by: Mhaylson Whandson <mhaylson@m2smart.com.br>

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -14,10 +14,15 @@ const buttonVariants = {
 };
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-  width: 180px;
+  width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme["green-500"]};
+  color: ${props => props.theme.white}
+
   /* ${props => {
     return css`
     background-color: ${buttonVariants[props.variant]}
